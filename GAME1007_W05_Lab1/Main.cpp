5740ab77f469773b2caaeba9718af583b9c904d3
TheProfezzzor <TheProfezzzor@users.noreply.github.com>
2021-10-04 15:07:57 -0400

    Created all basic functions.

--- GAME1007_W05_Lab1/Main.cpp
+++ GAME1007_W05_Lab1/Main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 using namespace std;
 
+// HandleEvents function. Handles events.
+void HandleEvents()
+{
+	cout << "Handling events..." << endl;
+}
+
+// Update function. Processes assets.
+void Update()
+{
+	cout << "Processing..." << endl;
+}
+
+// Render function. Updating the screen.
+void Render()
+{
+	cout << "Rendering..." << endl;
+}
+
 // Run function. Contains main game loop.
 int Run()
 {
+	bool isRunning = true;
+	char input;
+	while (isRunning)
+	{
+		HandleEvents(); // Input.
+		Update(); // Processing.
+		Render(); // Output.
+		cout << "Do you want to quit? [y/n]: ";
+		cin >> input;
+		if (input == 'y')
+			isRunning = false;
+	}
 	return 0;
 }
 
