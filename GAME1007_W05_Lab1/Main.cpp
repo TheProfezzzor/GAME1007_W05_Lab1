68fc2ff19834a42b3948cf5833fde2c8edf8939c
TheProfezzzor <TheProfezzzor@users.noreply.github.com>
2021-10-04 14:46:21 -0400

    Added main and Run functions.

---
+++ GAME1007_W05_Lab1/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Run function. Contains main game loop.
+int Run()
+{
+	return 0;
+}
+
+// main function. Entry point in every C++ program.
+int main()
+{
+	return Run();
+}
